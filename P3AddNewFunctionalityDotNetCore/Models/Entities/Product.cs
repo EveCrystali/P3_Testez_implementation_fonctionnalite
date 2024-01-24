e9e1b622e85070329607c600816621e1cc0d0b63
EveCrystali <34349789+EveCrystali@users.noreply.github.com>
2024-01-24 19:55:36 +0100

    refactor(ProductViewModel): add model validation using Data Annotations in ProductViewModel
    - remove the example method as specified in the TODO
    - implement model validation in ProductViewModel using Data Annotations for more robust and maintainable validation

--- P3AddNewFunctionalityDotNetCore/Models/Entities/Product.cs
+++ P3AddNewFunctionalityDotNetCore/Models/Entities/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace P3AddNewFunctionalityDotNetCore.Models.Entities
 {
@@ -10,12 +11,33 @@ namespace P3AddNewFunctionalityDotNetCore.Models.Entities
         }
 
         public int Id { get; set; }
+
         public string Description { get; set; }
+
         public string Details { get; set; }
+
+        [Required(ErrorMessage = "MissingName")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "MissingPrice")]
+        [RegularExpression(@"^\d+.\d{0,2}$", ErrorMessage = "InvalidPriceFormat")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "PriceNotGreaterThanZero")]
+
         public double Price { get; set; }
+
+
+        [Required(ErrorMessage = "MissingQuantity")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "InvalidQuantityFormat")]
+        [Range(1, int.MaxValue, ErrorMessage = "StockNotGreaterThanZero")]
         public int Quantity { get; set; }
 
         public virtual ICollection<OrderLine> OrderLine { get; set; }
+
+
     }
+
+
+  
+
+    
 }
